64fab7a14ea08cd663f73f6ea856dc503c68fa04
DESKTOP-BMOEAS8\Kawin <kawinsirikhanarat@gmail.com>
2017-04-23 15:48:14 +0700

    init git

---
+++ WrapitApplication/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WrapitApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // appPanel
+            // 
+            this.appPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.appPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.appPanel.Location = new System.Drawing.Point(0, 0);
+            this.appPanel.Name = "appPanel";
+            this.appPanel.Size = new System.Drawing.Size(284, 261);
+            this.appPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.appPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel appPanel;
+    }
+}
+
---
+++ WrapitApplication/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace WrapitApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCef();
+        }
+
+        void InitializeCef()
+        {
+            var settings = new CefSettings();
+            settings.RegisterScheme(new CefCustomScheme
+            {
+                SchemeName = CefSharpSchemeHandlerFactory.SchemeName,
+                SchemeHandlerFactory = new CefSharpSchemeHandlerFactory()
+            });
+            Cef.Initialize(settings);
+
+            var browser = new ChromiumWebBrowser("wrapper://app/bomber/index.html")
+            {
+                Dock = DockStyle.Fill
+            };
+            browser.ConsoleMessage += BrowserOnConsoleMessage;
+            appPanel.Controls.Add(browser);
+
+        }
+
+        private void BrowserOnConsoleMessage(object sender, ConsoleMessageEventArgs consoleMessageEventArgs)
+        {
+            Console.WriteLine("Browser Log : "+consoleMessageEventArgs.Message);
+        }
+    }
+
+
+
+    public class CefSharpSchemeHandler : ResourceHandler
+    {
+        public override bool ProcessRequestAsync(IRequest request, ICallback callback)
+        {
+            var uri = new Uri(request.Url);
+            Console.WriteLine("Direct to "+ request.Url);
+            var fileName = request.Url.Substring(request.Url.IndexOf("://") + 2);
+            Task.Run(() =>
+            {
+                using (callback)
+                {
+                    Stream stream = null;
+
+                    //if (string.Equals(fileName, "/PostDataTest.html", StringComparison.OrdinalIgnoreCase))
+                    //{
+                    //    var postDataElement = request.PostData.Elements.FirstOrDefault();
+                    //    stream = ResourceHandler.GetMemoryStream("Post Data: " + (postDataElement == null ? "null" : postDataElement.GetBody()), Encoding.UTF8);
+                    //}
+
+                    //if (string.Equals(fileName, "/PostDataAjaxTest.html", StringComparison.OrdinalIgnoreCase))
+                    //{
+                    //    var postData = request.PostData;
+                    //    if (postData == null)
+                    //    {
+                    //        stream = ResourceHandler.GetMemoryStream("Post Data: null", Encoding.UTF8);
+                    //    }
+                    //    else
+                    //    {
+                    //        var postDataElement = postData.Elements.FirstOrDefault();
+                    //        stream = ResourceHandler.GetMemoryStream("Post Data: " + (postDataElement == null ? "null" : postDataElement.GetBody()), Encoding.UTF8);
+                    //    }
+                    //}
+
+                    if (File.Exists(Application.StartupPath + fileName))
+                    {
+
+                        stream = new FileStream(Application.StartupPath + fileName, FileMode.Open);
+                        var fileExtension = Path.GetExtension(fileName);
+                        MimeType = ResourceHandler.GetMimeType(fileExtension);
+
+                    }
+
+                    if (stream == null)
+                    {
+                        callback.Cancel();
+                    }
+                    else
+                    {
+                        //Reset the stream position to 0 so the stream can be copied into the underlying unmanaged buffer
+                        stream.Position = 0;
+                        //Populate the response values - No longer need to implement GetResponseHeaders (unless you need to perform a redirect)
+                        ResponseLength = stream.Length;
+                        StatusCode = (int)HttpStatusCode.OK;
+                        Stream = stream;
+                        callback.Continue();
+                    }
+                }
+            });
+
+            return true;
+            
+        }
+    }
+
+    public class CefSharpSchemeHandlerFactory : ISchemeHandlerFactory
+    {
+        public const string SchemeName = "wrapper";
+
+        public IResourceHandler Create(IBrowser browser, IFrame frame, string schemeName, IRequest request)
+        {
+            var uri = new Uri(request.Url);
+            var fileName = request.Url.Substring(request.Url.IndexOf("://")+2);
+            string resource;
+            if (schemeName == SchemeName)
+            {
+                return new CefSharpSchemeHandler();
+
+            }
+            return null;
+        }
+    }
+}
